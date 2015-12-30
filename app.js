32f074f8ba4a7e81a6f7854b92cc48fa7e60d509
Rory <rory88@gmail.com>
2015-12-30 22:24:09 +0000

    added style and some (limited) functionality

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
+const Header = () => {
+    return (
+        <div className="col-xs-12">
+            <div className="col-xs-8">
+                <h6>FreeCodeCamp</h6>
+
+                <h1>100</h1>
+            </div>
+            <div className="col-xs-4">
+                <select name="boobs" id="">
+                    <option value="">All time</option>
+                    <option value="">Past 30 days</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
 const Panels = (props) => {
-        const arr = [1,2,3,4,5,6,7,8]
-        const lines =  arr.map((e, i) => {
-                return (
-                    <li className="panel" key={i}>
-                        <div className="panel-div">
-                            Hello
-                        </div>
-                    </li>
-                )
-            })
+    const {listOfCampers} = props;
+    console.log('list from panels:', listOfCampers);
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+    const lines = listOfCampers.map((e, i) => {
+        return (
+            <li className="panel" key={i}>
+                <div className="panel-inner">
+                    <img className="img-responsive" src={e.img}/>
+
+                    <div className="number">
+                        <h5>{i + 1}</h5>
+                    </div>
+                    <div className="info">
+                        <h5>{e.username}</h5>
+
+                        <p>Past 30 Days: {e.totalRecent}</p>
+
+                        <p>All time: {e.total}</p>
+                    </div>
+                </div>
+            </li>
+        )
+    })
     return (
 
         <div>
@@ -20,33 +51,41 @@ const Panels = (props) => {
     )
 }
 
+const Loading = () => <div><h1>Loading...</h1></div>
+
 const Main = React.createClass({
 
-	getInitialState() {
-		return {
-			data: []
-		}
-	},
+    getInitialState() {
+        return {
+            data: []
+        }
+    },
 
-	componentDidMount() {
-	    this.getCampers()
-	},
+    componentDidMount() {
+        this.getCampers()
+    },
 
-	getCampers() {
-		const url = 'http://fcctop100.herokuapp.com/api/fccusers/recent/:sortColumnName';
-		$.getJSON(url, data => {
-			console.log(data)
-		})
-	},
+    getCampers() {
+        const urlPastThirty = 'http://fcctop100.herokuapp.com/api/fccusers/recent/:sortColumnName';
+        const urlAllTime = 'http://fcctop100.herokuapp.com/api/fccusers/alltime/:sortColumnName.';
+        $.getJSON(urlPastThirty, dataFromSource => {
+            this.setState({data: dataFromSource});
+        })
+    },
 
-	render(){
+    render(){
         const {data} = this.state;
-		return (
-			<div>
-				<Panels listOfCampers={data} />
-			</div>
-		)
-	}
+        const loading = data.length === 0;
+        return (
+            <div>
+                <Header />
+                {loading
+                    ? <Loading />
+                    : <Panels listOfCampers={data}/>
+                }
+            </div>
+        )
+    }
 })
 
 ReactDOM.render(<Main />, document.getElementById('root'))
